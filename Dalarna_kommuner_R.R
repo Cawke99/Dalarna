20b11bccb2ebbb1db149e96b18342583d2a9091a
livsethbring <sethbring@outlook.com>
2023-11-07 10:37:30 +0100

    Lagt till ett script

--- Dalarna_kommuner_R.R
+++ Dalarna_kommuner_R.R
@@ -1,1 +1,17 @@
 #Här ska vi göra R script Dalarna Kommun
+library(sf)
+library(mapview)
+library(dplyr)
+
+
+kommun_fil <- "C:/Users/sethb/Svag_is_2/svag_is_2/Dalarna_kommuner.shp"
+
+dalarna_kommun <- st_read(kommun_fil, crs = 3006)
+
+mapview(dalarna_kommun)
+
+befolkn_fil <- "C:/Users/sethb/Svag_is_2/svag_is_2/bef_dalarna_23_70.csv"
+
+befolkning <- read.csv(befolkn_fil, header = TRUE, sep = ";", dec = ".")
+
+View(befolkning)

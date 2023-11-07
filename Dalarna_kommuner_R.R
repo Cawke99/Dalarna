7d11ee3475a8eb3729a4651f31377d6af16c2af6
livsethbring <sethbring@outlook.com>
2023-11-07 11:04:09 +0100

    Har lagt till buffer 30000

--- Dalarna_kommuner_R.R
+++ Dalarna_kommuner_R.R
@@ -14,8 +14,16 @@ dalarna_kommun <- st_read(kommun_fil, crs = 3006)
 
 mapview(dalarna_kommun)
 
-befolkn_fil <- "C:/Users/sethb/Svag_is_2/svag_is_2/bef_dalarna_23_70.csv"
+# Buffer ska vara över dalarna men blev kommunvis
+dalarna_buffer <- st_buffer(dalarna_kommun, 30000)
 
-befolkning <- read.csv(befolkn_fil, header = TRUE, sep = ";", dec = ".")
+mapview(dalarna_buffer)
 
-View(befolkning)
+
+# Fortsätter här när vi har en tabell som vi kan joina
+
+# befolkn_fil <- "C:/Users/sethb/Svag_is_2/svag_is_2/bef_dalarna_23_70.csv"
+# 
+# befolkning <- read.csv(befolkn_fil, header = TRUE, sep = ";", dec = ".")
+# 
+# View(befolkning)
